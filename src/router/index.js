c6a5df6c6a379372ae56f25f17f262d466057bc9
metehanadanur <77002704+metehanadanur@users.noreply.github.com>
2022-01-09 13:27:59 +0100

    Übersicht erstellt

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Uebersicht from '..@/views/Uebersicht'
-import Rechnung from '..@/views/Rechnung'
+import Uebersicht from '@/views/Uebersicht'
+import Rechnung from '@/views/Rechnung'
 import Firmen from '@/views/Firmen'
 
 const routes = [
