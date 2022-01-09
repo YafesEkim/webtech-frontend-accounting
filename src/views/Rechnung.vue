fc6722f19afe53297444069d0a09a391dd9e90ba
YafesEkim <yafes_ekim@hotmail.de>
2022-01-09 20:04:14 +0100

    Rechnung + Uebersicht

--- src/views/Rechnung.vue
+++ src/views/Rechnung.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container">
     <h1 style="text-align: center;">Hier können Sie ihre Rechnungen erstellen</h1>
-    <p>Tragen Sie bitte Ihre Rechnung in das untenstehende  <br >Formular ein.</p>
+    <p>Tragen Sie bitte Ihre Rechnung in das untenstehende <br>Formular ein.</p>
     <form>
       <label for="firma">Firmenname</label>
-      <input type="text" id="firma" name="firma" placeholder="Firma" required>
+      <input type="text" id="firma" name="firma" placeholder="Firma">
+      <label for="rechnungsNummer">Rechnungs Nummer</label>
+      <input type="text" id="rechnungsNummer" name="rechnungsNummer" placeholder="Rechnungs Nummer" required
+             v-model="rechnungsNummer">
       <label for="summe">Betrag €</label> <br>
-      <input type="number" id="summe" name="summe" placeholder="0.00" step="any" required> <br> <br>
+      <input type="number" id="summe" name="summe" placeholder="0.00" step="any" required v-model="betrag"> <br> <br>
       <label for="rechnungsdatum">Rechnungsdatum</label>
-      <input type="date" name="rechnungsdatum" id="rechnungsdatum">
+      <input type="date" name="rechnungsdatum" id="rechnungsdatum" required v-model="rechnungsDatum">
       <label for="art">Rechnungsart</label>
-      <select id="art" name="rechnungsart" required>
+      <select id="art" name="rechnungsart" required v-model="rechnungsart">
         <option value="" disabled selected hidden>Wähle aus...</option>
-        <option value="einnahme">Einnahme</option>
-        <option value="ausgabe">Ausgabe</option>
+        <option value="EINNAHME">Einnahme</option>
+        <option value="AUSGABE">Ausgabe</option>
       </select>
-      <label for="steuer">Mehrwertsteuersatz</label>
-      <select name="steuer" id="steuer" required>
-        <option value="" disabled selected hidden>Wähle aus...</option>
-        <option value="7mwst">7% MwSt.</option>
-        <option value="19mwst">19% MwSt</option>
-      </select>
-      <input type="submit" value="Rechnung erstellen">
+      <!--      <label for="steuer">Mehrwertsteuersatz</label>-->
+      <!--      <select name="steuer" id="steuer" required>-->
+      <!--        <option value="" disabled selected hidden>Wähle aus...</option>-->
+      <!--        <option value="7mwst">7% MwSt.</option>-->
+      <!--        <option value="19mwst">19% MwSt</option>-->
+      <!--      </select>-->
+      <input type="submit" value="Rechnung erstellen" @click='createRechnung'>
       <input type="reset" value="Eingaben löschen">
     </form>
   </div>
@@ -29,33 +32,81 @@
 
 <script>
 export default {
-  name: 'Rechnung'
+  name: 'Rechnung',
+  data () {
+    return {
+      firma: [],
+      rechnungsNummer: '',
+      rechnungsart: '',
+      rechnungsDatum: '',
+      betrag: '',
+      owner_id: 1
+    }
+  },
+  methods: {
+    createRechnung () {
+      const myHeaders = new Headers()
+      myHeaders.append('Content-Type', 'application/json')
+
+      const raw = JSON.stringify({
+        rechnungsNummer: this.rechnungsNummer,
+        rechnungsart: this.rechnungsart,
+        rechnungsDatum: this.rechnungsDatum,
+        betrag: this.betrag,
+        ownerId: this.owner_id
+      })
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      }
+
+      fetch('http://localhost:8080/api/v1/rechnungen/', requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
+    },
+    getFirma () {
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+
+      fetch('http://localhost:8080/api/v1/firma/', requestOptions)
+        .then(response => response.text())
+        .then(result => result.forEach(firma => this.firma.push(this.firma)))
+        .catch(error => console.log('error', error))
+    }
+  }
 }
 </script>
 
 <style scoped>
 
-body{
+body {
   background-color: rgb(174, 201, 77);
-  background-image:
-    url("../assets/buero-einrichten-t.jpg");
+  background-image: url("../assets/buero-einrichten-t.jpg");
   background-repeat: no-repeat;
-  -webkit-background-size : cover;
+  -webkit-background-size: cover;
   -moz-background-size: cover;
   -moz-background-size: cover;
 
 }
 
 h1 {
-  color: #4C6D8D ;
+  color: #4C6D8D;
 }
-p{
+
+p {
   text-align: center;
   font-size: larger;
   font-weight: bold;
   font-style: italic;
 }
-div{
+
+div {
   border-radius: 10px;
   background-color: #DAD7C5;
   padding: 10px;
@@ -63,7 +114,7 @@ div{
 
 }
 
-input[type=text] , input[type=password], input[type=number], input[type=date] , select {
+input[type=text], input[type=password], input[type=number], input[type=date], select {
   width: 100%;
   padding: 10px 15px;
   margin: 10px 0px;
@@ -90,7 +141,7 @@ input[type=submit]:hover {
 
 }
 
-input[type=text] , input[type=number], select {
+input[type=text], input[type=number], select {
   width: 100%;
   padding: 10px 15px;
   margin: 10px 0px;
@@ -113,12 +164,11 @@ label {
   width: 60%;
 }
 
-body{
+body {
   background-color: rgb(174, 201, 77);
-  background-image:
-    url("../assets/buero-einrichten-t.jpg");
+  background-image: url("../assets/buero-einrichten-t.jpg");
   background-repeat: no-repeat;
-  -webkit-background-size : cover;
+  -webkit-background-size: cover;
   -moz-background-size: cover;
   -moz-background-size: cover;
 
--- src/views/Uebersicht.vue
+++ src/views/Uebersicht.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="uebersicht">
     <h1>Übersicht</h1>
-    <div class="col" v-for="rechnung in rechnung" :key="rechnung.id">
+    <div class="col">
       <table class="table">
         <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Firma</th>
-          <th scope="col">Datum</th>
-          <th scope="col">Betrag in €</th>
+          <th>Firma</th>
+          <th>Datum</th>
+          <th>Betrag in €</th>
         </tr>
         </thead>
-        <tbody>
-        <tr>
-          <th scope="row">1</th>
+        <tbody v-for="rechnung in rechnung" :key="rechnung.id">
+        <tr v-if="rechnung.rechnunngsart === EINNAHME" class = "einnahme">
+          <td>{{rechnung.firma.firmaName}}</td>
+          <td>{{rechnung.rechnungsDatum}}</td>
+          <td>{{rechnung.betrag}}</td>
+        </tr>
+        <tr v-else-if="rechnung.rechnunngsart === AUSGABE" class = "ausgabe">
           <td>{{rechnung.firma.firmaName}}</td>
           <td>{{rechnung.rechnungsDatum}}</td>
           <td>{{rechnung.betrag}}</td>
@@ -37,15 +40,21 @@ export default {
       method: 'GET',
       redirect: 'follow'
     }
-    fetch('https://webtech1accounting.herokuapp.com//api/v1/rechnungen/', requestOptions)
+    fetch('http://localhost:8080/api/v1/rechnungen/', requestOptions)
       .then(response => response.json())
       .then(result => result.forEach(rechnung => this.rechnung.push(rechnung)))
       .catch(error => console.log('error', error))
   }
-
 }
+
 </script>
 
 <style>
+.einnahme{
+  background: darkseagreen;
+}
+.ausgabe{
+  background: indianred;
+}
 
 </style>
